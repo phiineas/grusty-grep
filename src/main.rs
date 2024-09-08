f53637f8598580abc83a8ebc6cf3d0726610bc0d
phiineas <geek.jainil18@gmail.com>
2024-09-09 01:18:04 +0530

    match digits

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ use std::process;
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
         return input_line.contains(pattern);
+    } else if pattern == "\\d" {
+        return input_line.contains(|c: char| c.is_digit(10));
     } else {
         panic!("Unhandled pattern: {}", pattern)
     }
 }
 
-// Usage: echo <input_text> | your_program.sh -E <pattern>
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
+// usage- echo <input_text> | your_program.sh -E <pattern>
 
+fn main() {
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
         process::exit(1);
@@ -25,7 +25,6 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    // Uncomment this block to pass the first stage
     if match_pattern(&input_line, &pattern) {
         process::exit(0)
     } else {
