2b4f9d3a2cb1324f10987d070ef72b37bd880ffd
phiineas <geek.jainil18@gmail.com>
2024-09-13 09:40:09 +0530

    negative character groups

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,9 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
     } else if pattern.starts_with('[') && pattern.ends_with(']') {
         let mut new_pattern = pattern.trim_matches('[').trim_matches(']').bytes();
         input_line.bytes().any(|val| new_pattern.any(|p| val == p))
+    } else if pattern.starts_with('[') && pattern.ends_with('[') {
+        let mut new_pattern = pattern.trim_matches('[').trim_matches(']').bytes();
+        input_line.bytes().any(|val| new_pattern.any(|p| val != p))
     } else {
         panic!("Unhandled pattern: {}", pattern)
     }
