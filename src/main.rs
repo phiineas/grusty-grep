e91379cf4a6dac97bfee1b2bd83c4c2746bd2fc4
phiineas <geek.jainil18@gmail.com>
2024-09-14 05:56:02 +0530

    negative character groups try-3

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
     } else if pattern == "\\w" {
         return input_line.contains(|c: char| c.is_alphanumeric());
     } else if pattern.starts_with('[') && pattern.ends_with(']') {
-        let new_pattern = pattern.trim_matches('[').trim_matches(']').bytes();
+        let mut new_pattern = pattern.trim_matches('[').trim_matches(']').bytes();
         input_line.bytes().any(|val| new_pattern.any(|p| val == p))
     } else if pattern.starts_with('[') && pattern.ends_with(']') {
         let new_pattern_1 = &pattern[2..pattern.len() - 1];
