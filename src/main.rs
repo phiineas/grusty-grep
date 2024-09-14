0e9180e94992f4b049713f564d9f9aebfbe4f1d7
phiineas <geek.jainil18@gmail.com>
2024-09-14 05:43:08 +0530

    negative character groups try-2

--- src/main.rs
+++ src/main.rs
@@ -23,19 +23,19 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
 // usage- echo <input_text> | your_program.sh -E <pattern>
 
 fn main() {
-    if env::args().nth(1).unwrap() != "-E" {
-        println!("Expected first argument to be '-E'");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 || args[1] != "-E" {
+        println!("Usage: your_program -E <pattern>");
         process::exit(1);
     }
 
-    let pattern = env::args().nth(2).unwrap();
-    let mut input_line = String::new();
+    let pattern = &args[2];
+    let stdin = io::stdin();
+    let input_line = stdin.lock().lines().next().unwrap().unwrap();
 
-    io::stdin().read_line(&mut input_line).unwrap();
-
-    if match_pattern(&input_line, &pattern) {
-        process::exit(0)
+    if match_pattern(&input_line, pattern) {
+        process::exit(0);
     } else {
-        process::exit(1)
+        process::exit(1);
     }
 }
